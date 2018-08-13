4106c64c59134ea9c6cfac36d527c78f09a8b6ff
Beulah Babu <beulah@mobikasa.com>
2018-08-13 15:59:47 +0530

    undefined db

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 var mongodb = require('mongodb').MongoClient;
 
 exports.connection = function(req,res,callback){
-	const url = "mongodb://ds117422.mlab.com/17422/instamongodb";
+	const url = "mongodb://BeulahBabu:Beulah123!@ds117422.mlab.com:17422/instamongodb";
 
 	mongodb.connect(url,{useNewUrlParser: true},function(err,database){
 		if(err){
