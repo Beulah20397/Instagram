aec2c53ce8506e0c0360bad864f0968cca84f438
Beulah Babu <beulah@mobikasa.com>
2018-08-13 13:41:31 +0530

    undefined db

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ var mongodb = require('mongodb').MongoClient;
 
 exports.connection = function(req,res,callback){
 	const url = "mongodb://ds117422.mlab.com/17422";
-
+	let db;
 	mongodb.connect(url,{useNewUrlParser: true},function(err,database){
 		if(err){
 			throw err;
